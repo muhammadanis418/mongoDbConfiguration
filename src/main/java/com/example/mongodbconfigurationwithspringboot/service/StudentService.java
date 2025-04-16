d43cdc5c619a511c761c1b2da252cb0f44261053
muani <muani@ciklum.com>
2025-04-16 15:45:33 +0500

    implement pagination and also fetch document by sub document field

--- src/main/java/com/example/mongodbconfigurationwithspringboot/service/StudentService.java
+++ src/main/java/com/example/mongodbconfigurationwithspringboot/service/StudentService.java
@@ -7,7 +7,14 @@ import java.util.List;
 public interface StudentService {
     Student save(Student student);
     Student findById(String id);
-    List<Student> findAll();
+    List<Student> findAll(int pageNo, int pageSize);
     Student update(Student student);
     String delete(String id);
+    List<Student>findByName(String name);
+    Student findByNameAndEmail(String name, String email);
+    Student findByNameOrEmail(String name, String email);
+
+    List<Student> findBYDepartmentByDepartmentName(String departmentName);
+
+    List<Student> findBySubjectSubjectName(String subjectName);
 }
